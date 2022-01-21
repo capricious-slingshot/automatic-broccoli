92bca454add50db6f4b62c8472ec48d7877023bc
capricious-slingshot <ephemere@protonmail.com>
2022-01-21 15:36:57 -0800

    error handling with node

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const rect = require('./rectangle')
 function solveRect(l, w) {
   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`)
 
-  if (l <= 0 || w <= 0) {
-    console.log(`Rectangle dimension must be greater than zero. Recieved: ${l}, ${w}`)
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}`)
-    console.log(`Perimiter of rectangle: ${rect.perimiter(l, w)}`)
-  }
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log('ERROR:', err.message)
+    } else {
+      console.log(`Area of rectangle with dimensions: ${rectangle.area()}`)
+      console.log(`Perimiter of rectangle with dimensions: ${rectangle.perimiter()}`)
+    }
+  })
+  console.log('This statement is logged after the call to rect() ')
 }
 
 solveRect(2, 4)
